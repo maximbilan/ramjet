ccf07ca3e056b743a2bb21e56b59b60f37b145b9
Maksym Bilan <maximb.mail@gmail.com>
2026-01-01 21:06:44 +0200

    Update formula revision to match v0.1.0 tag

--- Formula/ramjet.rb
+++ Formula/ramjet.rb
@@ -8,8 +8,7 @@ class Ramjet < Formula
   desc "Fast, lightweight CLI tool for macOS that reports system-wide RAM usage using Mach APIs"
   homepage "https://github.com/maximbilan/ramjet"
   url "https://github.com/maximbilan/ramjet.git",
-      tag:      "v0.1.0",
-      revision: "1ca4a4c333ddf0c20e7b23deb9e03836a6400876"
+      tag:      "v0.1.0"
   license "MIT"
   head "https://github.com/maximbilan/ramjet.git", branch: "main"
 
