ef03db993d117c8fdfc3ea36ad9eac4fb05cd4f2
Maksym Bilan <maximb.mail@gmail.com>
2026-01-03 12:14:19 +0200

    Brew 0.1.1 (#10)

--- Formula/ramjet.rb
+++ Formula/ramjet.rb
@@ -8,7 +8,7 @@ class Ramjet < Formula
   desc "Fast, lightweight CLI tool for macOS that reports system-wide RAM usage using Mach APIs"
   homepage "https://github.com/maximbilan/ramjet"
   url "https://github.com/maximbilan/ramjet.git",
-      tag:      "v0.1.0"
+      tag:      "v0.1.1"
   license "MIT"
   head "https://github.com/maximbilan/ramjet.git", branch: "main"
 
